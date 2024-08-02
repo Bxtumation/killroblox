2345bdc3e8869dd9319e0c4737f9b2f8a177b57f
Bxtumation <157051961+Bxtumation@users.noreply.github.com>
2024-08-02 13:34:32 +0300

    Add files via upload

---
+++ killroblox/cplusplus_dosyalari/cpp-source.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+#include <tlhelp32.h>
+
+bool KillProcessByName(const char* processName) {
+    HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hProcessSnap == INVALID_HANDLE_VALUE) return false;
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+    if (!Process32First(hProcessSnap, &pe32)) {
+        CloseHandle(hProcessSnap);
+        return false;
+    }
+    bool found = false;
+    do {
+        if (strcmp(pe32.szExeFile, processName) == 0) {
+            HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pe32.th32ProcessID);
+            if (hProcess != NULL) {
+                TerminateProcess(hProcess, 0);
+                CloseHandle(hProcess);
+                found = true;
+            }
+        }
+    } while (Process32Next(hProcessSnap, &pe32));
+    CloseHandle(hProcessSnap);
+    return found;
+}
+
+int main() {
+    KillProcessByName("RobloxPlayerBeta.exe");
+}
